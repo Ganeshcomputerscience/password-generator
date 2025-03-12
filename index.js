3d5fa1d65fd58879d48c274c6ce7f4643fae0f2d
Ganeshcomputerscience <125945449+Ganeshcomputerscience@users.noreply.github.com>
2025-03-12 15:59:04 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let characters = ["A","B","C","D","E","F","G",
+    "H","I","J" ,"K","L","M","N","O","P","Q","R",
+    "S","T","U","V","W","X","Y","Z","a","b","c","d",
+    "e","f","g","h","i","j","k","l","m","n","o","p",
+    "q","r","s","t","u","v","w","x","y","z", "0", "1",
+     "2", "3", "4", "5", "6", "7", "8", "9","~","`","!",
+     "@","#","$","%","^","&","*","(",")","_","-","+","=",
+     "{","[","}","]",",","|",":",";","<",">",".","?","/"];
+     let passEl=document.getElementById("gen1-el")
+     let genEl=document.getElementById("gen2-el")
+     let paraEl=document.getElementById("para-el")
+
+     function paraEl(){
+        paraEl.textContent="Ganesh"
+     }
